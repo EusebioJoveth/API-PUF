5772616658baf7e8e863ce33c5f36d021e6becb1
Eusébio Joveth <yurijoveth@gmail.com>
2022-12-02 14:12:24 +0100

    feat: users routes

---
+++ src/modules/users/index.js
@@ -0,0 +1,15 @@
+export const list = (ctx) =>{
+    ctx.body = 'list Eusebio!'
+}
+
+export const create =(ctx) =>{
+    ctx.body = 'Create users'
+}
+
+export const update = (ctx) =>{
+    ctx.body = 'Update users'
+}
+
+export const remove = (ctx) =>{
+    ctx.body = 'Remove Users'
+}
